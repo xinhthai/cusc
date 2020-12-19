10f7bb5af3cbdd06f998a096c7ceb31c78cb9b2b
duy <loivang.nhatduy@gmail.com>
2020-12-19 22:57:37 +0700

    change image directory

--- src/main/java/com/myxinh/cusc/web/constants/SystemConstants.java
+++ src/main/java/com/myxinh/cusc/web/constants/SystemConstants.java
@@ -2,5 +2,5 @@ package com.myxinh.cusc.web.constants;
 
 public class SystemConstants {
     public static final String BASE_URL = "localhost:3000/api";
-    public static final String IMAGE_DIRECTORY = "C:\\Users\\asus s510u\\Desktop\\";
+    public static final String IMAGE_DIRECTORY = "D:\\cusc_image";
 }
